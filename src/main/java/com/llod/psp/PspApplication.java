374e17be78f1f44b1622cb40ec3d98801d746ca3
llod24 <llodhigh@gmail.com>
2024-10-01 17:57:58 +0900

    first commit

---
+++ src/main/java/com/llod/psp/PspApplication.java
@@ -0,0 +1,13 @@
+package com.llod.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llod/psp/PspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llod.psp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
